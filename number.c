87f70888cd4c56ec05f603ffd6d108b231615a40
AhmedCoder <AhmedattiaCoder@gmail.com>
2022-12-09 00:35:13 +0200

    created files

---
+++ number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    int y =(int)x%2;
+    if(y==0){
+    printf("even");        
+  
+   
+      }  
+ else{
+    printf("odd");    
+    }
+return 0; 
+    
+}
